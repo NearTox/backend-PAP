8c133ca32ef2c586e4c48f2204887e811f43c2ca
NearTox <NearTox@outlook.com>
2016-12-15 21:38:16 -0600

    change pre save to post save

--- core/signals.py
+++ core/signals.py
@@ -3,7 +3,7 @@
 # import the logging library
 import logging
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Orden
@@ -12,9 +12,9 @@ from .tasks import calc_precio
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-@receiver(pre_save, sender=Orden)
+@receiver(post_save, sender=Orden)
 def send_email_per_item(sender, instance, **kwargs):
-  if sender.precio == 0:
+  if instance.precio == 0:
     #calc_precio.delay(sender)
-    calc_precio(sender)
+    calc_precio(instance)
     logger.info("Se calculara el precio")
